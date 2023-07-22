d666534ba335df967d50da521d67165b8523b412
Levi H <100445409+lh1207@users.noreply.github.com>
2023-07-22 00:53:09 -0400

    firebase premade login.html

--- src/main/java/com/vehicoolrentals/app/FirebaseInitializer.java
+++ src/main/java/com/vehicoolrentals/app/FirebaseInitializer.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
@@ -14,12 +15,12 @@ public class FirebaseInitializer {
 
     @PostConstruct
     public void initialize() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("resources/firebase-adminsdk.json");
+        ClassPathResource serviceAccountResource = new ClassPathResource("firebase-adminsdk.json");
+        FileInputStream serviceAccount = new FileInputStream(serviceAccountResource.getFile());
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://yehicool-rentals.firebaseio.com")
+                .setDatabaseUrl("https://vehicool-rentals.firebaseio.com")
                 .build();
 
         FirebaseApp.initializeApp(options);
