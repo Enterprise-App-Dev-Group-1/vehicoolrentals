1e84a05dc3c18e745ea0b7237e3e843b1f473b9d
Levi H <100445409+lh1207@users.noreply.github.com>
2023-07-22 02:16:16 -0400

    clean front end

--- src/main/java/com/vehicoolrentals/app/ProfileController.java
+++ src/main/java/com/vehicoolrentals/app/ProfileController.java
@@ -3,7 +3,6 @@ package com.vehicoolrentals.app;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
-import com.vehicoolrentals.app.FirebaseUserController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
